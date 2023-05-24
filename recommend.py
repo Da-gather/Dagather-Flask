35317d161c239959e2aa79c7b9166e95fb5add4e
minjikim <inkmjj@sookmyung.ac.kr>
2023-05-25 04:02:58 +0900

    [del] 안쓰는 import 삭제

--- recommend.py
+++ recommend.py
@@ -1,6 +1,4 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import MinMaxScaler
